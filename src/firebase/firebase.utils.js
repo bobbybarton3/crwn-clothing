1d42942bb327f4f2749dd9ba77f1d67fcaaf7d2e
Bobby Barton <bobbybarton3@gmail.com>
2021-09-18 20:20:06 -0500

    Sign in and Sign up pages

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,7 +10,35 @@ const config = {
     messagingSenderId: "241812209342",
     appId: "1:241812209342:web:85bd1cc705751d0021a8ef",
     measurementId: "G-YC4RE54CCK"
-  };
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    console.log(snapShot)
+
+    if(!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+           await userRef.set({
+               displayName,
+               email,
+               createdAt,
+               ...additionalData
+           }) 
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+
+    return userRef;
+}
 
 firebase.initializeApp(config);
 
